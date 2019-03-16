9bffd3e68aef7dd68809dc7407bc5b79dc05968e
Alim Öncül <alimoncul@gmail.com>
2019-03-16 19:26:38 +0300

    Bug fixes, color palette update, emergency stop button added.

--- app/src/main/java/com/teamfire/picontroller/MainActivity.java
+++ app/src/main/java/com/teamfire/picontroller/MainActivity.java
@@ -1,7 +1,10 @@
 package com.teamfire.picontroller;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Handler;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,11 +22,12 @@ import java.net.UnknownHostException;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_down, btn_up, btn_left, btn_right, btn_connect;
+    Button btn_down, btn_up, btn_left, btn_right, btn_emergency;
     EditText ipAddress;
     public static String wifiModuleIP;
     public static int wifiModulePort;
     public static int CMD;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,32 +36,73 @@ public class MainActivity extends AppCompatActivity {
         btn_up = findViewById(R.id.btn_up);
         btn_left = findViewById(R.id.btn_left);
         btn_right = findViewById(R.id.btn_right);
-        btn_connect = findViewById(R.id.btn_connect);
+        btn_emergency = findViewById(R.id.btn_emergencyStop);
         ipAddress = findViewById(R.id.ipAddress);
 
 
-        btn_up.setOnTouchListener(new View.OnTouchListener() {
+        btn_emergency.setOnTouchListener(new View.OnTouchListener() {
             private Handler mHandler;
+
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                switch(event.getAction()) {
+                switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
                         if (mHandler != null) return true;
                         mHandler = new Handler();
+                        btn_emergency.setBackgroundTintList(ContextCompat.getColorStateList(MainActivity.this, R.color.emergencyPressed));
+                        mHandler.postDelayed(mAction, 50);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        if (mHandler == null) return true;
+                        mHandler.removeCallbacks(mAction);
+                        btn_emergency.setBackgroundTintList(ContextCompat.getColorStateList(MainActivity.this, R.color.emergency));
+                        CMD = 0;
+                        mHandler = null;
+                        break;
+                }
+                return false;
+            }
+
+            Runnable mAction = new Runnable() {
+                @Override
+                public void run() {
+                    getIPandPort();
+                    CMD = 0;
+                    SocketAsyncTask cmd_increase_servo = new SocketAsyncTask();
+                    cmd_increase_servo.execute();
+                    mHandler.postDelayed(this, 100);
+                }
+            };
+        });
+
+        btn_up.setOnTouchListener(new View.OnTouchListener() {
+            private Handler mHandler;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        if (mHandler != null) return true;
+                        mHandler = new Handler();
+                        btn_up.setBackgroundTintList(ContextCompat.getColorStateList(MainActivity.this, R.color.forwardPressed));
                         btn_down.setEnabled(false);
                         mHandler.postDelayed(mAction, 50);
                         break;
                     case MotionEvent.ACTION_UP:
                         if (mHandler == null) return true;
                         mHandler.removeCallbacks(mAction);
+                        btn_up.setBackgroundTintList(ContextCompat.getColorStateList(MainActivity.this, R.color.forward));
+                        CMD = 0;
                         btn_down.setEnabled(true);
                         mHandler = null;
                         break;
                 }
                 return false;
             }
+
             Runnable mAction = new Runnable() {
-                @Override public void run() {
+                @Override
+                public void run() {
                     getIPandPort();
                     CMD = 8;
                     SocketAsyncTask cmd_increase_servo = new SocketAsyncTask();
@@ -69,26 +114,32 @@ public class MainActivity extends AppCompatActivity {
 
         btn_down.setOnTouchListener(new View.OnTouchListener() {
             private Handler mHandler;
+
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                switch(event.getAction()) {
+                switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
                         if (mHandler != null) return true;
                         mHandler = new Handler();
+                        btn_down.setBackgroundTintList(ContextCompat.getColorStateList(MainActivity.this, R.color.backwardPressed));
                         btn_up.setEnabled(false);
                         mHandler.postDelayed(mAction, 50);
                         break;
                     case MotionEvent.ACTION_UP:
                         if (mHandler == null) return true;
                         mHandler.removeCallbacks(mAction);
-                        btn_down.setEnabled(true);
+                        btn_down.setBackgroundTintList(ContextCompat.getColorStateList(MainActivity.this, R.color.backward));
+                        CMD = 0;
+                        btn_up.setEnabled(true);
                         mHandler = null;
                         break;
                 }
                 return false;
             }
+
             Runnable mAction = new Runnable() {
-                @Override public void run() {
+                @Override
+                public void run() {
                     getIPandPort();
                     CMD = 2;
                     SocketAsyncTask cmd_increase_servo = new SocketAsyncTask();
@@ -100,26 +151,32 @@ public class MainActivity extends AppCompatActivity {
 
         btn_right.setOnTouchListener(new View.OnTouchListener() {
             private Handler mHandler;
+
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                switch(event.getAction()) {
+                switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
                         if (mHandler != null) return true;
                         mHandler = new Handler();
+                        btn_right.setBackgroundTintList(ContextCompat.getColorStateList(MainActivity.this, R.color.safePressed));
                         btn_left.setEnabled(false);
                         mHandler.postDelayed(mAction, 50);
                         break;
                     case MotionEvent.ACTION_UP:
                         if (mHandler == null) return true;
                         mHandler.removeCallbacks(mAction);
+                        btn_right.setBackgroundTintList(ContextCompat.getColorStateList(MainActivity.this, R.color.safe));
+                        CMD = 0;
                         btn_left.setEnabled(true);
                         mHandler = null;
                         break;
                 }
                 return false;
             }
+
             Runnable mAction = new Runnable() {
-                @Override public void run() {
+                @Override
+                public void run() {
                     getIPandPort();
                     CMD = 6;
                     SocketAsyncTask cmd_increase_servo = new SocketAsyncTask();
@@ -131,24 +188,32 @@ public class MainActivity extends AppCompatActivity {
 
         btn_left.setOnTouchListener(new View.OnTouchListener() {
             private Handler mHandler;
+
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                switch(event.getAction()) {
+                switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
                         if (mHandler != null) return true;
                         mHandler = new Handler();
+                        btn_left.setBackgroundTintList(ContextCompat.getColorStateList(MainActivity.this, R.color.safePressed));
+                        btn_right.setEnabled(false);
                         mHandler.postDelayed(mAction, 50);
                         break;
                     case MotionEvent.ACTION_UP:
                         if (mHandler == null) return true;
                         mHandler.removeCallbacks(mAction);
+                        btn_left.setBackgroundTintList(ContextCompat.getColorStateList(MainActivity.this, R.color.safe));
+                        CMD = 0;
+                        btn_right.setEnabled(true);
                         mHandler = null;
                         break;
                 }
                 return false;
             }
+
             Runnable mAction = new Runnable() {
-                @Override public void run() {
+                @Override
+                public void run() {
                     getIPandPort();
                     CMD = 4;
                     SocketAsyncTask cmd_increase_servo = new SocketAsyncTask();
@@ -159,28 +224,30 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void getIPandPort()
-    {
+    public void getIPandPort() {
         String iPandPort = ipAddress.getText().toString();
-        String temp[]= iPandPort.split(":");
+        String temp[] = iPandPort.split(":");
         wifiModuleIP = temp[0];
         wifiModulePort = Integer.valueOf(temp[1]);
     }
 
-    public class SocketAsyncTask extends AsyncTask<Void,Void,Void>{
+    public class SocketAsyncTask extends AsyncTask<Void, Void, Void> {
         Socket socket;
 
         @Override
-        protected Void doInBackground(Void... params){
+        protected Void doInBackground(Void... params) {
             try {
-                Log.d("ASYNCBASLADI","IP:" );
                 InetAddress inetAddress = InetAddress.getByName(MainActivity.wifiModuleIP);
                 socket = new java.net.Socket(inetAddress, MainActivity.wifiModulePort);
                 PrintStream printStream = new PrintStream(socket.getOutputStream());
                 printStream.print(CMD);
                 printStream.close();
                 socket.close();
-            }catch (UnknownHostException e){e.printStackTrace();}catch (IOException e){e.printStackTrace();}
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             return null;
         }
     }
